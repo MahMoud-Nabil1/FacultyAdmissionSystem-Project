b2403e3680b9880ce8a757e9be541fa0ab7a7862
teracura <abdallahelbhary39@gmail.com>
2026-02-22 18:33:04 +0200

    Improve staff model
    details: made id of type string, made name trimmed, made role an enum of various roles, made email trimmed and lowercase, added timestamps for update and create times, added hashing to the entity as well as find

--- server/models/staff.js
+++ server/models/staff.js
@@ -1,37 +1,83 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
-const {schema} = mongoose;
+const { Schema } = mongoose;
 
-const staffSchema = new schema({
+const staffSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
     name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     role: {
         type: String,
-        required: true
+        required: true,
+        enum: ['admin', 'academic_guide', 'academic_guide_coordinator', 'Reporter']
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
     },
-    hash:{
+    hash: {
         type: String,
-        required: true
+        required: true,
+        select: false
     },
-    salt:{
+    salt: {
         type: String,
+        select: false
     },
     departments: {
-        type: schema.Types.ObjectId,
-        ref: 'Departments'
+        type: [Schema.Types.ObjectId],
+        ref: 'Department'
     },
-    students: {
-        type: [schema.Types.ObjectId],
+    students: [{
+        type: Schema.Types.ObjectId,
         ref: 'Student'
+    }]
+}, { timestamps: true });
+
+staffSchema.virtual('password')
+    .set(function(password) {
+        this._password = password;
+    });
+
+staffSchema.pre('save', async function(next) {
+    if (!this._password) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.salt = salt;
+        this.hash = await bcrypt.hash(this._password, salt);
+        next();
+    } catch (err) {
+        next(err);
     }
-})
+});
+
+staffSchema.pre('validate', function(next) {
+    if (this.isNew && !this._password) {
+        this.invalidate('password', 'Password is required');
+    }
+    next();
+});
+
+staffSchema.methods.verifyPassword = function(password) {
+    return bcrypt.compare(password, this.hash);
+};
+
+staffSchema.pre(/^find/, function(next) {
+    console.log("Staff query:", this.getQuery());
+    next();
+});
 
 const Staff = mongoose.model('Staff', staffSchema);
-
 export default Staff;
